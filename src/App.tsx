26920e0f97ee9648a1649964610838947034d516
Chimbay <c.anthony.priv@gmail.com>
2025-09-26 11:56:30 -0500

    Initial framework

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import type { Component } from 'solid-js'
+
+const App: Component = () => {
+  return (
+    <div class="min-h-screen flex items-center justify-center bg-gray-100">
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
+
+export default App

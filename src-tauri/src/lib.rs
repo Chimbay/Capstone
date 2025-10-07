6d9635d8b42c76ee7ebec9501da4acd8cbfb31db
Chimbay <c.anthony.priv@gmail.com>
2025-10-07 12:29:10 -0500

    Initial prototype

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,18 +1,51 @@
+use tauri_plugin_log::{Target, TargetKind, log};
+
+use lopdf::Document;
+use std::path::Path;
+
+#[tauri::command]
+fn pdf_to_text(file_bytes: Vec<u8>) -> Result<String, String> {
+    let doc = Document::load_mem(&file_bytes).map_err(|e| e.to_string())?;
+    let page_numbers: Vec<u32> = doc.get_pages().keys().copied().collect();
+    let text = doc.extract_text(&page_numbers).map_err(|e| e.to_string())?.replace("\n", " ");
+
+    Ok(text)
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    let mut builder = tauri::Builder::default();
-
+    let builder = tauri::Builder::default();
     // Enable the Tauri devtools plugin in development builds
-    #[cfg(debug_assertions)]
-    {
-        let devtools = tauri_plugin_devtools::init();
-        builder = builder.plugin(devtools);
-    }
+    // #[cfg(debug_assertions)]
+    // {
+    //     let devtools = tauri_plugin_devtools::init();
+    //     builder = builder.plugin(devtools);
+    // }
+
 
     builder
-        // .plugin( /* Add your Tauri plugin here */ )
-        // Add your commands here that you will call from your JS code
-        // .invoke_handler(tauri::generate_handler![ /* Add your commands here */ ])
+        .plugin(
+            tauri_plugin_log::Builder::new()
+                .target(Target::new(TargetKind::Stdout))
+                .level(log::LevelFilter::Info)
+                .filter(|metadata| metadata.target().starts_with("webview:"))
+                .format(|out, message, record| {
+                    let target = record
+                        .target()
+                        .strip_prefix("webview:")
+                        .unwrap_or(record.target());
+
+                    let file_part = target.split('@').nth(1).unwrap_or(target);
+
+                    let filename = Path::new(file_part)
+                        .file_name()
+                        .and_then(|f| f.to_str())
+                        .unwrap_or(file_part);
+                    out.finish(format_args!("[{}] {}", filename, message))
+                })
+                .build(),
+        )
+        .invoke_handler(tauri::generate_handler![pdf_to_text])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import FileInput from '@components/file_input'
 import type { Component } from 'solid-js'
 
 const App: Component = () => {
   return (
     <div class="min-h-screen flex items-center justify-center bg-gray-100">
-      <div></div>
-      <div></div>
+      
+      <FileInput/>
     </div>
   )
 }

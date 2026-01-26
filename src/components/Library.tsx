288ebb52518096a7269c1b55eb0b82defe365e2b
Chimbay <c.anthony.priv@gmail.com>
2026-01-26 16:32:05 -0600

    Library UI adjusted

--- src/components/Library.tsx
+++ src/components/Library.tsx
@@ -1,7 +1,7 @@
 import { DocumentAPI } from '@api/document'
 import { useNavigate } from '@solidjs/router'
 import { useToast } from '@ui/toast/ToastContext'
-import { createResource, For, Show } from 'solid-js'
+import { createResource, createSignal, For, Show } from 'solid-js'
 
 interface FileMetadata {
   uuid: string
@@ -12,20 +12,37 @@ interface FileMetadata {
 }
 
 function ItemInfo(props: { data: FileMetadata }) {
-  return <>{JSON.stringify(props.data)}</>
+  return (
+    <div class="absolute bg-white border p-2 shadow-lg rounded">
+      <ul>
+        <li>{JSON.stringify(props.data, null, 2)}</li>
+      </ul>
+    </div>
+  )
 }
 function Item(props: { file: FileMetadata }) {
+  const [showInfo, setShowInfo] = createSignal(false)
   const navigate = useNavigate()
+
   function open_file() {
     navigate(`/view/${props.file.uuid}`, { replace: true })
   }
   return (
-    <>
-      <>{<ItemInfo data={props.file} />}</>
-      <button class="border 1px" onClick={open_file}>
+    <div class="relative border 1p">
+      <div class="" onClick={() => setShowInfo(!showInfo())}>
+        Info
+      </div>
+      <Show when={showInfo()}>
+        <ItemInfo data={props.file} />
+      </Show>
+      <div class="flex flex-col items-center" onClick={open_file}>
+        <img
+          src="/preview-a6.svg"
+          class="justify-center items-center w-25 aspect-[1/1.4] p-1"
+        />
         {props.file.display_name}
-      </button>
-    </>
+      </div>
+    </div>
   )
 }
 
